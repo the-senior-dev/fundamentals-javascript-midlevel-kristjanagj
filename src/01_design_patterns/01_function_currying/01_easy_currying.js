55fae9f4d879e78d74d76ae4660eb6b3335c6435
Kristi <kristi.gjoka92@gmail.com>
2024-12-05 16:22:44 +0100

    design_patterns done

--- src/01_design_patterns/01_function_currying/01_easy_currying.js
+++ src/01_design_patterns/01_function_currying/01_easy_currying.js
@@ -9,7 +9,11 @@
 // curriedAdd(1)(2); // 3
 
 function curry(fn) {
-    // Your implementation here
+    return function curriedFunc(a){
+        return function curriedFunc2(b){
+            return fn(a,b);
+        }
+    }
 }
 
 module.exports = curry;
